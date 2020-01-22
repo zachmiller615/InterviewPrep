988f91f2b6977ff543d3d933797ed772c5428e4a
Zach Miller <zach.miller615@gmail.com>
2020-01-22 18:39:40 +0200

    Working on heap add method

--- InterviewPrepCore/Sources/InterviewPrepCore/Heap/Heap.swift
+++ InterviewPrepCore/Sources/InterviewPrepCore/Heap/Heap.swift
@@ -18,16 +18,34 @@ class Heap<ElementType> where ElementType: Comparable {
     }
 
     func add(_ element: ElementType) {
-        guard (!self.data.isEmpty) else {
-            self.data.append(element)
-            return
+        self.data.append(element)
+        let elementIndex = (self.data.count - 1)
+        let parentIndex = self.parentIndex(childIndex: elementIndex)
+        let elementShouldSwimUp = self.elementShouldSwimUp(element: element, parentIndex: parentIndex)
+        if elementShouldSwimUp {
+
+        } else {
+
         }
-
-
     }
 }
 
+// Private Methods
+private extension Heap {
+    func elementShouldSwimUp(element: ElementType, parentIndex: Int) -> Bool {
+        switch self.type {
+        case .min:
+            return (element < self.data[parentIndex])
+        case .max:
+            return (element > self.data[parentIndex])
+        }
+    }
 
+    func parentIndex(childIndex: Int) -> Int {
+        let potentialParentIndex = (childIndex / 2)
+        return potentialParentIndex
+    }
+}
 
 
 
