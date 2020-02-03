035f0018d4b126d98220af28da835007bef09d3a
Zach Miller <zach.miller615@gmail.com>
2020-02-03 14:31:15 +0200

    Edit to command line tool

--- InterviewPrepCore/Sources/InterviewPrepCore/Command_Line_Tool/CommandLineTool.swift
+++ InterviewPrepCore/Sources/InterviewPrepCore/Command_Line_Tool/CommandLineTool.swift
@@ -8,26 +8,6 @@ public struct CommandLineTool {
 
     // Methods
     public func run() {
-        let dynamicProgramming = DynamicProgramming()
-        let numberOfRows = 17
-        let numberOfColumns = 9
-        let offLimitCells: Set<GridPoint> =
-                [GridPoint(row: 0, column: 7),
-                 GridPoint(row: 0, column: 8),
-                 GridPoint(row: 1, column: 8),
-                 GridPoint(row: 2, column: 8),
-                 GridPoint(row: 3, column: 8),
-                 GridPoint(row: 4, column: 8),
-                 GridPoint(row: 5, column: 8),
-                 GridPoint(row: 6, column: 8),
-                 GridPoint(row: 7, column: 8),
-                 GridPoint(row: 8, column: 8),
-                 GridPoint(row: 9, column: 7),
-                 GridPoint(row: 9, column: 8)]
-        print("-----")
-        let result = dynamicProgramming.robotInAGrid(numberOfRows: numberOfRows, numberOfColumns: numberOfColumns, offLimitCells: offLimitCells)
-        print("-----")
-        print("Result: \(String(describing: result))")
-        print("-----")
+
     }
 }
