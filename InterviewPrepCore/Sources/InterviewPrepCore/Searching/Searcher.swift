b1fb6fecc70f477a42bafd220a3e2ebe25867856
Zach Miller <zach.miller615@gmail.com>
2020-02-12 06:52:16 +0200

    Added searcher method: searchInRotatedArray()

--- InterviewPrepCore/Sources/InterviewPrepCore/Searching/Searcher.swift
+++ InterviewPrepCore/Sources/InterviewPrepCore/Searching/Searcher.swift
@@ -24,6 +24,36 @@ struct Searcher {
     func binarySearchRecursive<Element: Comparable>(sortedArray: [Element], target: Element) -> Int? {
         self.binarySearchRecursive(sortedArray: sortedArray, target: target, low: 0, high: (sortedArray.count - 1))
     }
+
+    /// Question from "Cracking the Coding Interview" by Gayle Laakmann Mcdowell:
+    /// Given a sorted array of n integers that has been rotated an unknown number of times, write code to find an element in the array.
+    /// You may assume that the array was originally sorted in increasing order.
+    func searchInRotatedArray<Element: Comparable>(_ array: [Element], target: Element) -> Int? {
+        var low = 0
+        var high = (array.count - 1)
+        var middle = ((low + high) / 2)
+        while (low <= high) {
+            middle = ((low + high) / 2)
+            if (array[middle] == target) {
+                return middle
+            }
+            let shouldSearchLeft: Bool
+            let rotationPointIsLeft = (array[low] > array[middle])
+            if (rotationPointIsLeft) {
+                let targetFitsOnRightSide = ((array[middle] < target) && (target <= array[high]))
+                shouldSearchLeft = !targetFitsOnRightSide
+            } else {
+                let targetFitsOnLeftSide = ((array[low] <= target) && (target < array[middle]))
+                shouldSearchLeft = targetFitsOnLeftSide
+            }
+            if shouldSearchLeft {
+                high = (middle - 1)
+            } else {
+                low = (middle + 1)
+            }
+        }
+        return nil
+    }
 }
 
 // Private Methods
---
+++ InterviewPrepCore/Tests/InterviewPrepCoreTests/Searching/SearcherTests.swift
@@ -0,0 +1,197 @@
+//
+// Created by Zach Miller on 30/01/2020.
+//
+
+import XCTest
+@testable import InterviewPrepCore
+
+class SearcherTests: XCTestCase {
+}
+
+// Test Cases for Binary Search Iterative
+extension SearcherTests {
+    func test_BinarySearchIterative_BasicCase() {
+        // Given
+        let searcher = Searcher()
+        let sortedArray = [1, 2, 3, 4, 5, 6, 7]
+        let target = 6
+        let expectedResult = 5
+
+        // When
+        let computedResult = searcher.binarySearchIterative(sortedArray: sortedArray, target: target)
+
+        // Then
+        XCTAssertEqual(computedResult, expectedResult)
+    }
+
+    func test_BinarySearchIterative_NilCase() {
+        // Given
+        let searcher = Searcher()
+        let sortedArray = [1, 2, 3, 4, 5, 6, 7]
+        let target = 8
+
+        // When
+        let computedResult = searcher.binarySearchIterative(sortedArray: sortedArray, target: target)
+
+        // Then
+        XCTAssertNil(computedResult)
+    }
+}
+
+// Test Cases for Binary Search Iterative
+extension SearcherTests {
+    func test_BinarySearchRecursive_BasicCase() {
+        // Given
+        let searcher = Searcher()
+        let sortedArray = [1, 2, 3, 4, 5, 6, 7]
+        let target = 6
+        let expectedResult = 5
+
+        // When
+        let computedResult = searcher.binarySearchRecursive(sortedArray: sortedArray, target: target)
+
+        // Then
+        XCTAssertEqual(computedResult, expectedResult)
+    }
+
+    func test_BinarySearchRecursive_NilCase() {
+        // Given
+        let searcher = Searcher()
+        let sortedArray = [1, 2, 3, 4, 5, 6, 7]
+        let target = 8
+
+        // When
+        let computedResult = searcher.binarySearchRecursive(sortedArray: sortedArray, target: target)
+
+        // Then
+        XCTAssertNil(computedResult)
+    }
+}
+
+// Test Cases for Search In Rotated Array
+extension SearcherTests {
+    func test_SearchInRotatedArray_NilCase() {
+        // Given
+        let searcher = Searcher()
+        let array = [20, 25, 1, 3, 4, 5, 7, 10]
+        let target = 30
+
+        // When
+        let computedResult = searcher.searchInRotatedArray(array, target: target)
+
+        // Then
+        XCTAssertNil(computedResult)
+    }
+
+    func test_SearchInRotatedArray_RotationPointIsLeft_TargetIsLeft() {
+        // Given
+        let searcher = Searcher()
+        let array = [20, 25, 1, 3, 4, 5, 7, 10]
+        let target = 25
+        let expectedResult = 1
+
+        // When
+        let computedResult = searcher.searchInRotatedArray(array, target: target)
+
+        // Then
+        XCTAssertEqual(computedResult, expectedResult)
+    }
+
+    func test_SearchInRotatedArray_RotationPointIsLeft_TargetIsRight() {
+        // Given
+        let searcher = Searcher()
+        let array = [20, 25, 1, 3, 4, 5, 7, 10]
+        let target = 7
+        let expectedResult = 6
+
+        // When
+        let computedResult = searcher.searchInRotatedArray(array, target: target)
+
+        // Then
+        XCTAssertEqual(computedResult, expectedResult)
+    }
+
+    func test_SearchInRotatedArray_RotationPointIsRight_TargetIsLeft() {
+        // Given
+        let searcher = Searcher()
+        let array = [15, 16, 19, 20, 25, 1, 3]
+        let target = 19
+        let expectedResult = 2
+
+        // When
+        let computedResult = searcher.searchInRotatedArray(array, target: target)
+
+        // Then
+        XCTAssertEqual(computedResult, expectedResult)
+    }
+
+    func test_SearchInRotatedArray_RotationPointIsRight_TargetIsRight() {
+        // Given
+        let searcher = Searcher()
+        let array = [15, 16, 19, 20, 25, 1, 3]
+        let target = 3
+        let expectedResult = 6
+
+        // When
+        let computedResult = searcher.searchInRotatedArray(array, target: target)
+
+        // Then
+        XCTAssertEqual(computedResult, expectedResult)
+    }
+
+    func test_SearchInRotatedArray_RotationPointIsCenter_TargetIsLeft() {
+        // Given
+        let searcher = Searcher()
+        let array = [15, 20, 25, 1, 3, 4, 5]
+        let target = 25
+        let expectedResult = 2
+
+        // When
+        let computedResult = searcher.searchInRotatedArray(array, target: target)
+
+        // Then
+        XCTAssertEqual(computedResult, expectedResult)
+    }
+
+    func test_SearchInRotatedArray_RotationPointIsCenter_TargetIsRight() {
+        // Given
+        let searcher = Searcher()
+        let array = [15, 20, 25, 1, 3, 4, 5]
+        let target = 3
+        let expectedResult = 4
+
+        // When
+        let computedResult = searcher.searchInRotatedArray(array, target: target)
+
+        // Then
+        XCTAssertEqual(computedResult, expectedResult)
+    }
+
+    func test_SearchInRotatedArray_LeftSideAllDuplicates() {
+        // Given
+        let searcher = Searcher()
+        let array = [2, 2, 2, 3, 4, 2]
+        let target = 3
+        let expectedResult = 3
+
+        // When
+        let computedResult = searcher.searchInRotatedArray(array, target: target)
+
+        // Then
+        XCTAssertEqual(computedResult, expectedResult)
+    }
+
+    func test_SearchInRotatedArray_RightSideAllDuplicates() {
+        // Given
+        let searcher = Searcher()
+        let array = [5, 3, 4, 5, 5, 5]
+        let target = 3
+        let expectedResult = 1
+
+        // When
+        let computedResult = searcher.searchInRotatedArray(array, target: target)
+
+        // Then
+        XCTAssertEqual(computedResult, expectedResult)
+    }
+}
